4436a5e29fe662a80dd35767ba416606fa1266f6
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2006-06-26 16:57:48 +0000

    prepare for initial distros
    git-svn-id: http://svn.zope.org/repos/main/zc.buildout/trunk@68843 62d5b8a3-27da-0310-9561-8e5933582275

--- testrunnerrecipe/setup.py
+++ testrunnerrecipe/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "zc.recipe.testrunner",
-    version = "0.1",
+    version = "1.0",
     packages = find_packages('src'),
     include_package_data = True,
     package_dir = {'':'src'},
