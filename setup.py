4973b090adaa7ca521ba75b8f5f62255b1a717d6
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2006-06-26 21:35:45 +0000

    improved meta data
    git-svn-id: http://svn.zope.org/repos/main/zc.buildout/trunk@68861 62d5b8a3-27da-0310-9561-8e5933582275

--- setup.py
+++ setup.py
@@ -3,17 +3,21 @@ from setuptools import setup, find_packages
 setup(
     name = "zc.buildout",
     version = "1.0",
+    author = "Jim Fulton",
+    author_email = "jim@zope.com",
+    description = "System for managing development buildouts",
+    license = "ZPL 2.1",
+    keywords = "development build",
+    url='http://svn.zope.org/zc.buildout',
+    long_description=open('README.txt').read(),
+
+
     packages = ['zc', 'zc.buildout'],
     package_dir = {'': 'src'},
     namespace_packages = ['zc'],
     include_package_data = True,
     tests_require = ['zope.testing'],
     test_suite = 'zc.buildout.tests.test_suite',
-    author = "Jim Fulton",
-    author_email = "jim@zope.com",
-    description = "System for managing development buildouts",
-    license = "ZPL 2.1",
-    keywords = "development build",
     install_requires = 'setuptools',
     entry_points = {'console_scripts':
                     ['buildout = zc.buildout.buildout:main']}, 
