801e1f11df17923062ab7eb0e3b2c4540a919330
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2006-06-27 11:12:19 +0000

    Updated ignoes and fixed setup metadata.
    git-svn-id: http://svn.zope.org/repos/main/zc.buildout/trunk@68867 62d5b8a3-27da-0310-9561-8e5933582275

--- testrunnerrecipe/setup.py
+++ testrunnerrecipe/setup.py
@@ -3,6 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name = "zc.recipe.testrunner",
     version = "1.0",
+    author = "Jim Fulton",
+    author_email = "jim@zope.com",
+    description = "ZC Buildout recipe for creating test runners",
+    license = "ZPL 2.1",
+    keywords = "development build",
+    url='http://svn.zope.org/zc.buildout',
+
     packages = find_packages('src'),
     include_package_data = True,
     package_dir = {'':'src'},
@@ -10,11 +17,6 @@ setup(
     install_requires = ['zc.buildout', 'zope.testing', 'setuptools'],
     dependency_links = ['http://download.zope.org/distribution/'],
     test_suite = 'zc.recipe.testrunner.tests.test_suite',
-    author = "Jim Fulton",
-    author_email = "jim@zope.com",
-    description = "ZC Buildout recipe for creating test runners",
-    license = "ZPL 2.1",
-    keywords = "development build",
     entry_points = {'zc.buildout':
                     ['default = zc.recipe.testrunner:TestRunner']},
     )
