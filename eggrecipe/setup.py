8ec1c010cef00770c36659d98bc10649ab9d2ab4
Jim Fulton <jim@zope.com>
2006-06-27 11:12:19 +0000

    Updated ignoes and fixed setup metadata.

--- eggrecipe/setup.py
+++ eggrecipe/setup.py
@@ -3,6 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name = "zc.recipe.egg",
     version = "1.0",
+    author = "Jim Fulton",
+    author_email = "jim@zope.com",
+    description = "Recipe for installing Python package distributions as eggs",
+    license = "ZPL 2.1",
+    keywords = "development build",
+    url='http://svn.zope.org/zc.buildout',
+
     packages = find_packages('src'),
     include_package_data = True,
     package_dir = {'':'src'},
@@ -10,10 +17,5 @@ setup(
     install_requires = ['zc.buildout', 'setuptools'],
     tests_require = ['zope.testing'],
     test_suite = 'zc.recipe.eggs.tests.test_suite',
-    author = "Jim Fulton",
-    author_email = "jim@zope.com",
-    description = "Recipe for installing Python package distributions as eggs",
-    license = "ZPL 2.1",
-    keywords = "development build",
     entry_points = {'zc.buildout': ['default = zc.recipe.egg:Egg']},    
     )
