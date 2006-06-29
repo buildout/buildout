c6a6112f363da46c1f08b15f429b0ba4934d69ae
jim <jim@62d5b8a3-27da-0310-9561-8e5933582275>
2006-06-29 20:37:14 +0000

    Misc adjustments.
    git-svn-id: http://svn.zope.org/repos/main/zc.buildout/trunk@68917 62d5b8a3-27da-0310-9561-8e5933582275

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 from setuptools import setup, find_packages
 
+name = "zc.buildout"
 setup(
-    name = "zc.buildout",
-    version = "1.0.dev",
+    name = name,
+    version = "1.0.0a1",
     author = "Jim Fulton",
     author_email = "jim@zope.com",
     description = "System for managing development buildouts",
+    long_description=open('README.txt').read(),
     license = "ZPL 2.1",
     keywords = "development build",
     url='http://svn.zope.org/zc.buildout',
-    download_url='http://download.zope.org/distribution',
-    long_description=open('README.txt').read(),
-
 
     packages = ['zc', 'zc.buildout'],
     package_dir = {'': 'src'},
     namespace_packages = ['zc'],
+    install_requires = 'setuptools',
     include_package_data = True,
     tests_require = ['zope.testing'],
-    test_suite = 'zc.buildout.tests.test_suite',
-    install_requires = 'setuptools',
+    test_suite = name+'.tests.test_suite',
     entry_points = {'console_scripts':
-                    ['buildout = zc.buildout.buildout:main']}, 
+                    ['buildout = %s.buildout:main' % name]}, 
+    dependency_links = ['http://download.zope.org/distribution/'],
     )
--- zc.recipe.testrunner/setup.py
+++ zc.recipe.testrunner/setup.py
@@ -1,23 +1,25 @@
 from setuptools import setup, find_packages
 
+name = "zc.recipe.testrunner"
 setup(
-    name = "zc.recipe.testrunner",
-    version = "1.0.dev",
+    name = name,
+    version = "1.0.0a1",
     author = "Jim Fulton",
     author_email = "jim@zope.com",
     description = "ZC Buildout recipe for creating test runners",
+    long_description=open('README.txt').read(),
     license = "ZPL 2.1",
-    keywords = "development build",
+    keywords = "development build testing",
     url='http://svn.zope.org/zc.buildout',
-    download_url='http://download.zope.org/distribution',
 
     packages = find_packages('src'),
     include_package_data = True,
     package_dir = {'':'src'},
     namespace_packages = ['zc', 'zc.recipe'],
-    install_requires = ['zc.buildout', 'zope.testing', 'setuptools'],
+    install_requires = ['zc.buildout', 'zope.testing', 'setuptools',
+                        'zc.recipe.egg',
+                        ],
+    test_suite = name+'.tests.test_suite',
+    entry_points = {'zc.buildout': ['default = %s:TestRunner' % name]},
     dependency_links = ['http://download.zope.org/distribution/'],
-    test_suite = 'zc.recipe.testrunner.tests.test_suite',
-    entry_points = {'zc.buildout':
-                    ['default = zc.recipe.testrunner:TestRunner']},
     )
