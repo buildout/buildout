91350c625968524d9107b6a28b6b77076d988e83
Jim Fulton <jim@zope.com>
2006-09-15 10:48:12 +0000

    Normalize sample test execution times.

--- zc.recipe.testrunner/src/zc/recipe/testrunner/tests.py
+++ zc.recipe.testrunner/src/zc/recipe/testrunner/tests.py
@@ -58,6 +58,7 @@ def test_suite():
                (re.compile('\S+sample-(\w+)'), r'/sample-\1'),
                (re.compile('-([^-]+)-py\d[.]\d.egg'), r'-py2.3.egg'),
                (re.compile(r'\\+'), '/'),
+               (re.compile('\d[.]\d+ seconds'), '0.001 seconds')
                ])
             ),
         
