627f5859a9e738cd24ec43f563b86fa73bb83057
Jim Fulton <jim@zope.com>
2006-10-01 15:15:15 +0000

    Updated release info.

--- zc.recipe.testrunner/setup.py
+++ zc.recipe.testrunner/setup.py
@@ -1,13 +1,24 @@
+import os
 from setuptools import setup, find_packages
 
 name = "zc.recipe.testrunner"
 setup(
     name = name,
-    version = "1.0.0b1",
+    version = "1.0.0b2",
     author = "Jim Fulton",
     author_email = "jim@zope.com",
     description = "ZC Buildout recipe for creating test runners",
-    long_description=open('README.txt').read(),
+    long_description = (
+        open('README.txt').read()
+         + '\n' +
+        open('CHANGES.txt').read()
+        + '\n' +
+        'Detailed Documentation\n'
+        '**********************\n'
+        + '\n' +
+        open(os.path.join('src', 'zc', 'recipe', 'testrunner', 'README.txt')
+             ).read()
+        ),
     license = "ZPL 2.1",
     keywords = "development build testing",
     url='http://svn.zope.org/zc.buildout',
@@ -16,7 +27,7 @@ setup(
     include_package_data = True,
     package_dir = {'':'src'},
     namespace_packages = ['zc', 'zc.recipe'],
-    install_requires = ['zc.buildout  >=1.0.0b3', 'zope.testing', 'setuptools',
+    install_requires = ['zc.buildout  >=1.0.0b7', 'zope.testing', 'setuptools',
                         'zc.recipe.egg  >=1.0.0a3',
                         ],
     test_suite = name+'.tests.test_suite',
