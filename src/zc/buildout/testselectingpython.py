cacc8addc3e31bb5c622f47464ed44b525ce1964
Jim Fulton <jim@zope.com>
2008-01-14 00:46:41 +0000

    Rearranged the selecting-python tests to make it possible to run some selecting Python tests without Python 2.3 available, since I've lost Python 2.3 in my upgrade to Mac OS X 2.5. :(

--- src/zc/buildout/testselectingpython.py
+++ src/zc/buildout/testselectingpython.py
@@ -11,36 +11,43 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-import os, re, unittest
+import os, re, sys, unittest
 from zope.testing import doctest, renormalizing
 import zc.buildout.tests
 import zc.buildout.testing
 
-def test_selecting_python_via_easy_install():
-    """\
+if sys.version_info[:2] == (2, 4):
+    other_version = "2.5"
+else:
+    other_version = "2.4"
 
+__test__ = dict(
+    test_selecting_python_via_easy_install=
+    """\
+    
 We can specify an specific Python executable.
 
     >>> dest = tmpdir('sample-install')
     >>> ws = zc.buildout.easy_install.install(
     ...     ['demo'], dest, links=[link_server],
     ...     index='http://www.python.org/pypi/',
-    ...     always_unzip=True, executable= python2_3_executable)
+    ...     always_unzip=True, executable= other_executable)
 
     >>> ls(dest)
-    d  demo-0.3-py2.3.egg
-    d  demoneeded-1.1-py2.3.egg
-    d  setuptools-0.6-py2.3.egg
+    d  demo-0.3-py%(other_version)s.egg
+    d  demoneeded-1.1-py%(other_version)s.egg
+    d  setuptools-0.6-py%(other_version)s.egg
 
-"""
+""" % dict(other_version=other_version)
+    )
 
 def multi_python(test):
-    p23 = zc.buildout.testing.find_python('2.3')
+    other_executable = zc.buildout.testing.find_python(other_version)
     sample_eggs = test.globs['tmpdir']('sample_eggs')
     os.mkdir(os.path.join(sample_eggs, 'index'))
     test.globs['sample_eggs'] = sample_eggs
-    zc.buildout.tests.create_sample_eggs(test, executable=p23)
-    test.globs['python2_3_executable'] = p23
+    zc.buildout.tests.create_sample_eggs(test, executable=other_executable)
+    test.globs['other_executable'] = other_executable
 
 
 def setup(test):
@@ -56,6 +63,7 @@ def test_suite():
         setUp=setup,
         tearDown=zc.buildout.testing.buildoutTearDown,
         checker=renormalizing.RENormalizing([
-            (re.compile('setuptools-\S+-py2.3.egg'), 'setuptools-V-py2.3.egg'),
+            (re.compile('setuptools-\S+-py%s.egg' % other_version),
+             'setuptools-V-py%s.egg' % other_version),
             ]),
         )
