e5ce9d6f17a3749e1a5efde6eab47c5c1ebd69eb
andreasjung <andreasjung@62d5b8a3-27da-0310-9561-8e5933582275>
2008-06-01 08:01:35 +0000

    providing useful default values in order to get around an
    AttributeError:
    Buildout instance has no attribute '_logger' 
    
    
    
    
    git-svn-id: http://svn.zope.org/repos/main/zc.buildout/trunk@87085 62d5b8a3-27da-0310-9561-8e5933582275

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-version = "1.0.3-dev"
+version = "1.0.3"
 
 import os
 from setuptools import setup, find_packages
