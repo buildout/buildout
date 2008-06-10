6d03418fee6066a5414510759644ec72022e4922
Andreas Jung <yet@gmx.de>
2008-06-10 09:00:16 +0000

    1.0.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-version = "1.0.5-dev"
+version = "1.0.5"
 
 import os
 from setuptools import setup, find_packages
