7e1857cc22748ec788c67ebfd3e8639e84ef4547
Jim Fulton <jim@zope.com>
2008-07-19 14:08:12 +0000

    set version.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 
-version = "1.0.7-dev"
+version = "1.1.0"
 
 import os
 from setuptools import setup, find_packages
