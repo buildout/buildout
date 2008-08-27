1b70fef0ce6f75571ef35d518930540c26021956
georgyberdyshev <georgyberdyshev@62d5b8a3-27da-0310-9561-8e5933582275>
2008-08-27 22:44:58 +0000

    Increased version number.
    Signed-off-by: Georgy Berdyshev - Георгий Бердышев <codingmaster@gmail.com>
    
    
    git-svn-id: http://svn.zope.org/repos/main/zc.buildout/trunk@90482 62d5b8a3-27da-0310-9561-8e5933582275

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 
-version = "1.1.1dev"
+version = "1.1.2dev"
 
 import os
 from setuptools import setup, find_packages
