9b52218211db1fb72d970d25367b53aee4d65d3c
tarek <tarek@62d5b8a3-27da-0310-9561-8e5933582275>
2009-10-23 09:23:37 +0000

    added the distribute option to the bootstrap script
    git-svn-id: http://svn.zope.org/repos/main/zc.buildout/trunk@105232 62d5b8a3-27da-0310-9561-8e5933582275

--- bootstrap/bootstrap.py
+++ bootstrap/bootstrap.py
@@ -21,20 +21,51 @@ $Id$
 """
 
 import os, shutil, sys, tempfile, urllib2
+from optparse import OptionParser
 
 tmpeggs = tempfile.mkdtemp()
 
 is_jython = sys.platform.startswith('java')
 
+# parsing arguments
+parser = OptionParser()
+parser.add_option("-v", "--version", dest="version",
+                          help="use a specific zc.buildout version")
+parser.add_option("-d", "--distribute",
+                   action="store_true", dest="distribute", default=False,
+                   help="Use Disribute rather than Setuptools.")
+
+options, args = parser.parse_args()
+
+if options.version is not None:
+    VERSION = '==%s' % options.version
+else:
+    VERSION = ''
+
+USE_DISTRIBUTE = options.distribute
+args = args + ['bootstrap']
+
+to_reload = False
 try:
     import pkg_resources
+    if not hasattr(pkg_resources, '_distribute'):
+        to_reload = True
+        raise ImportError
 except ImportError:
     ez = {}
-    exec urllib2.urlopen('http://peak.telecommunity.com/dist/ez_setup.py'
+    if USE_DISTRIBUTE:
+        exec urllib2.urlopen('http://python-distribute.org/distribute_setup.py'
                          ).read() in ez
-    ez['use_setuptools'](to_dir=tmpeggs, download_delay=0)
+        ez['use_setuptools'](to_dir=tmpeggs, download_delay=0, no_fake=True)
+    else:
+        exec urllib2.urlopen('http://peak.telecommunity.com/dist/ez_setup.py'
+                             ).read() in ez
+        ez['use_setuptools'](to_dir=tmpeggs, download_delay=0)
 
-    import pkg_resources
+    if to_reload:
+        reload(pkg_resources)
+    else:
+        import pkg_resources
 
 if sys.platform == 'win32':
     def quote(c):
@@ -49,12 +80,10 @@ else:
 cmd = 'from setuptools.command.easy_install import main; main()'
 ws  = pkg_resources.working_set
 
-if len(sys.argv) > 2 and sys.argv[1] == '--version':
-    VERSION = '==%s' % sys.argv[2]
-    args = sys.argv[3:] + ['bootstrap']
+if USE_DISTRIBUTE:
+    requirement = 'distribute'
 else:
-    VERSION = ''
-    args = sys.argv[1:] + ['bootstrap']
+    requirement = 'setuptools'
 
 if is_jython:
     import subprocess
@@ -63,7 +92,7 @@ if is_jython:
            quote(tmpeggs), 'zc.buildout' + VERSION],
            env=dict(os.environ,
                PYTHONPATH=
-               ws.find(pkg_resources.Requirement.parse('setuptools')).location
+               ws.find(pkg_resources.Requirement.parse(requirement)).location
                ),
            ).wait() == 0
 
@@ -73,7 +102,7 @@ else:
         '-c', quote (cmd), '-mqNxd', quote (tmpeggs), 'zc.buildout' + VERSION,
         dict(os.environ,
             PYTHONPATH=
-            ws.find(pkg_resources.Requirement.parse('setuptools')).location
+            ws.find(pkg_resources.Requirement.parse(requirement)).location
             ),
         ) == 0
 
