da5135c1904fae0f0e3013ef8fe86ed9aaf3751c
Gary Poster <gary.poster@canonical.com>
2010-08-29 20:51:23 +0000

    prep for release

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 name = "zc.buildout"
-version = "1.5.1dev"
+version = "1.5.1"
 
 import os
 from setuptools import setup
