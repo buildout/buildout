68a7f7a4b5c273a4b4fe20576379c6f3c41d7f3b
icemac <icemac@62d5b8a3-27da-0310-9561-8e5933582275>
2011-02-28 07:47:08 +0000

    Conform to repository policy.
    git-svn-id: http://svn.zope.org/repos/main/zc.buildout/trunk@120582 62d5b8a3-27da-0310-9561-8e5933582275

--- z3c.recipe.scripts_/src/z3c/recipe/scripts/scripts.py
+++ z3c.recipe.scripts_/src/z3c/recipe/scripts/scripts.py
@@ -1,6 +1,6 @@
 ##############################################################################
 #
-# Copyright (c) 2009-2010 Zope Corporation and Contributors.
+# Copyright (c) 2009-2010 Zope Foundation and Contributors.
 # All Rights Reserved.
 #
 # This software is subject to the provisions of the Zope Public License,
