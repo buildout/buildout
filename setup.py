3cbdfb08b57e84087845d73fde6427e62a5954fb
Jim Fulton <jim@zope.com>
2011-04-30 12:09:35 +0000

    *** empty log message ***

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 name = "zc.buildout"
-version = "2.0.0a1-dev"
+version = "2.0.0a1"
 
 import os
 from setuptools import setup
