1fab5d8025a8a30f8575df3d7a0e573ae76b51e8
Domen Kožar <domen@dev.si>
2012-08-18 22:12:22 +0200

    version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 name = "zc.buildout"
-version = "1.6.0"
+version = "1.6.1"
 
 import os
 from setuptools import setup
