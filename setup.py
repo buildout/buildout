610444e7b59e0ce108768039279325c624481fdf
Jim Fulton <jim@zope.com>
2012-11-19 11:16:21 -0500

    new rev for doc fix

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 name = "zc.buildout"
-version = "2.0.0a3"
+version = "2.0.0a4"
 
 import os
 from setuptools import setup
