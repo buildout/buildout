efcfa8cdd34e800c979d1db2ba5b51838a8a81dd
Jim Fulton <jim@zope.com>
2012-12-01 13:04:33 -0500

    version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 name = "zc.buildout"
-version = "2.0.0a4"
+version = "2.0.0a5"
 
 import os
 from setuptools import setup
