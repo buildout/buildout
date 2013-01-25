dadc1632269d0140bcaeb0b9c76d4553a95aed39
Reinout van Rees <reinout@vanrees.org>
2013-01-25 21:14:23 +0100

    Raising an error if buildout-versions or buildout.dumppickedversions is used

--- src/zc/buildout/buildout.py
+++ src/zc/buildout/buildout.py
@@ -960,6 +960,14 @@ class Buildout(DictMixin):
     def _load_extensions(self):
         __doing__ = 'Loading extensions.'
         specs = self['buildout'].get('extensions', '').split()
+        for superceded_extension in ['buildout-versions',
+                                     'buildout.dumppickedversions']:
+            if superceded_extension in specs:
+                msg = ("The extension %s is now included in buildout itself.\n"
+                       "Remove the extension from your configuration and "
+                       "look at the `show-picked-versions`\n"
+                       "option in buildout's documentation.")
+                raise zc.buildout.UserError(msg % superceded_extension)
         if specs:
             path = [self['buildout']['develop-eggs-directory']]
             if self.offline:
