64ab669129d5851bc73f997f1a189439bf6494b9
Tres Seaver <tseaver@palladion.com>
2013-06-09 23:33:40 -0400

    Drop the 'no_fake' argument when calling ez_setup.
    We don't need to juke faking setuptools (or not) when we are using
    setuptools rather than distribute.

--- bootstrap/bootstrap.py
+++ bootstrap/bootstrap.py
@@ -78,7 +78,7 @@ except ImportError:
     # XXX use a more permanent ez_setup.py URL when available.
     exec(urlopen('https://bitbucket.org/pypa/setuptools/raw/0.7.2/ez_setup.py'
                 ).read(), ez)
-    setup_args = dict(to_dir=tmpeggs, download_delay=0, no_fake=True)
+    setup_args = dict(to_dir=tmpeggs, download_delay=0)
     ez['use_setuptools'](**setup_args)
 
     if to_reload:
