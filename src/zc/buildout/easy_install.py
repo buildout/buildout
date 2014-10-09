6597f59996b869a2e4f435f73f4c851d0c842e6a
Peter Uittenbroek <uittenbroek@goldmund-wyldebeast-wunderliebe.com>
2014-10-09 12:39:33 +0200

    Rename destination to dest. Remove commented line

--- src/zc/buildout/easy_install.py
+++ src/zc/buildout/easy_install.py
@@ -672,8 +672,6 @@ class Installer:
                     pkg_resources.VersionConflict(dist, req), ws)
             requirements.extend(dist.requires(req.extras)[::-1])
             processed[req] = True
-            # if dist.location in self._site_packages:
-            #     logger.debug('Egg from site-packages: %s', dist)
         return ws
 
     def build(self, spec, build_ext):
