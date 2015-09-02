36408d63539fd0d950577d818557fb1790c4966d
Reinout van Rees <reinout@vanrees.org>
2015-09-03 00:04:47 +0200

    Back to development: 2.4.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 name = "zc.buildout"
-version = '2.4.3'
+version = '2.4.4.dev0'
 
 import os
 from setuptools import setup
