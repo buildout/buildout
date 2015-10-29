cc0c0d4f4777a8f26e601687be796ee43967e5c6
Reinout van Rees <reinout@vanrees.org>
2015-10-29 10:46:47 +0100

    Back to development: 2.4.8
    [ci skip]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 name = "zc.buildout"
-version = '2.4.7'
+version = '2.4.8.dev0'
 
 import os
 from setuptools import setup
