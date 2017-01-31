9c12d6a1a05a545ce54c6ae133d0cd825e10c489
Jim Fulton <jim@zope.com>
2017-01-31 12:10:43 -0500

    Back to development: 2.7.2
    [ci skip]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 name = "zc.buildout"
-version = '2.7.1'
+version = '2.7.2.dev0'
 
 import os
 from setuptools import setup
