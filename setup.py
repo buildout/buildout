3ed677fcc1ade06dd417f0b5a777fbc7fbd58f17
Jim Fulton <jim@jimfulton.info>
2017-03-06 12:57:18 -0500

    Back to development: 2.9.2
    [ci skip]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 name = "zc.buildout"
-version = '2.9.1'
+version = '2.9.2.dev0'
 
 import os
 from setuptools import setup
