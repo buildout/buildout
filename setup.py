633f0511ba278e718d4caccaf7ad0258a4a64eb9
Godefroid Chapelle <gotcha@bubblenet.be>
2017-03-30 12:15:16 +0200

    Back to development: 2.9.4
    [ci skip]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 name = "zc.buildout"
-version = '2.9.3'
+version = '2.9.4.dev0'
 
 import os
 from setuptools import setup
