d6c13b3edfb61b46a2a129104c3c23f282700c86
Reinout van Rees <reinout@vanrees.org>
2019-01-29 09:20:53 +0100

    Back to development: 2.13.2
    [ci skip]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 name = "zc.buildout"
-version = '2.13.1'
+version = '2.13.2.dev0'
 
 import os
 from setuptools import setup
