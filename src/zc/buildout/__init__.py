f52e42366637d5aae34dfceecd5c3be689fd9758
Godefroid Chapelle <gotcha@bubblenet.be>
2020-05-06 18:55:56 +0200

    make non-pinned install of setuptools much quicker
    by monkeypatching `pkg_resources.Distribution`

--- src/zc/buildout/__init__.py
+++ src/zc/buildout/__init__.py
@@ -30,6 +30,51 @@ from pip._vendor import six
 from pip._vendor.six.moves import urllib
 
 
+def patch_Distribution():
+    try:
+        from pkg_resources import _remove_md5_fragment
+        from pkg_resources import Distribution
+
+        if hasattr(Distribution, 'location'):
+            return
+
+        # prepare any Distribution built before monkeypatch
+        from pkg_resources import working_set
+        for dist in working_set:
+            dist._location = dist.location
+            dist._location_without_md5 = _remove_md5_fragment(dist.location)
+
+        def hashcmp(self):
+            without_md5 = getattr(self, '_location_without_md5', '')
+            return (
+                self.parsed_version,
+                self.precedence,
+                self.key,
+                without_md5,
+                self.py_version or '',
+                self.platform or '',
+            )
+
+        def get_location(self):
+            try:
+                result = self._location
+            except AttributeError:
+                result = ''
+            return result
+
+        def set_location(self, l):
+            self._location = l
+            self._location_without_md5 = _remove_md5_fragment(l)
+
+        setattr(Distribution, 'location', property(get_location, set_location))
+        setattr(Distribution, 'hashcmp', property(hashcmp))
+    except ImportError:
+        return
+
+
+patch_Distribution()
+
+
 def setup_coverage():
     if not 'RUN_COVERAGE' in os.environ:
         return
@@ -47,6 +92,10 @@ def setup_coverage():
 
 
 PY_VERSION_INFO = TargetPython().py_version_info
+
+
+
+
 
 
 def patch_PackageIndex():
