6ea3970caebaf0db5235a4d0ca67231ae07072c9
Godefroid Chapelle <gotcha@bubblenet.be>
2021-03-07 12:19:13 +0100

    Back to development: 3.0.0b2
    [ci skip]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 name = "zc.buildout"
-version = '3.0.0b1'
+version = '3.0.0b2.dev0'
 
 import os
 from setuptools import setup
