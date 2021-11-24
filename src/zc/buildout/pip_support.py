02ba9731290a11259cf0dd90eb06f0747a1c3512
Godefroid Chapelle <gotcha@bubblenet.be>
2021-11-24 15:55:32 +0100

    Problem: pip install return code is not returned
    Solution: pass it properly to buildout via a call to sys.exit

--- src/zc/buildout/pip_support.py
+++ src/zc/buildout/pip_support.py
@@ -1,8 +1,10 @@
-try: 
+try:
     # support pip>=19.0
     from pip._internal.cli.main import main
-    pip_install_cmd = 'from pip._internal.cli.main import main; main()'
+    pip_install_cmd = (
+        'from pip._internal.cli.main import main; sys.exit(main())'
+    )
 except ImportError:
     # support pip>=10.0
     from pip._internal import main
-    pip_install_cmd = 'from pip._internal import main; main()'
+    pip_install_cmd = 'from pip._internal import main; sys.exit(main())'
