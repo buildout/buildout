2713c8d6c863126b1e81a2b8b09f459c4cd7c107
Godefroid Chapelle <gotcha@bubblenet.be>
2022-11-06 16:44:02 +0100

    Claim python 3.11 support
    [skip ci]

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ setup(
        'Programming Language :: Python :: 3.8',
        'Programming Language :: Python :: 3.9',
        'Programming Language :: Python :: 3.10',
+       'Programming Language :: Python :: 3.11',
        'Topic :: Software Development :: Build Tools',
        'Topic :: Software Development :: Libraries :: Python Modules',
        ],
