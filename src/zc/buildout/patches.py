1904c2322665116864c3d6675cb7f2193d83dfe3
Maurits van Rees <maurits@vanrees.org>
2024-06-17 17:21:46 +0200

    Patches: catch more import errors and then do not apply that patch.

--- src/zc/buildout/patches.py
+++ src/zc/buildout/patches.py
@@ -17,30 +17,30 @@ def patch_Distribution():
     try:
         from pkg_resources import Distribution
         from .compat import version
-
-        def hashcmp(self):
-            if hasattr(self, '_hashcmp'):
-                return self._hashcmp
-            else:
-                try:
-                    parsed_version = self.parsed_version
-                except version.InvalidVersion:
-                    # You get here when there is an distribution on PyPI
-                    # with a version that is no longer seen as valid.
-                    parsed_version = version.Version("0.0.0")
-                self._hashcmp = result = (
-                    parsed_version,
-                    self.precedence,
-                    self.key,
-                    self.location,
-                    self.py_version or '',
-                    self.platform or '',
-                )
-                return result
-
-        setattr(Distribution, 'hashcmp', property(hashcmp))
     except ImportError:
         return
+
+    def hashcmp(self):
+        if hasattr(self, '_hashcmp'):
+            return self._hashcmp
+        else:
+            try:
+                parsed_version = self.parsed_version
+            except version.InvalidVersion:
+                # You get here when there is an distribution on PyPI
+                # with a version that is no longer seen as valid.
+                parsed_version = version.Version("0.0.0")
+            self._hashcmp = result = (
+                parsed_version,
+                self.precedence,
+                self.key,
+                self.location,
+                self.py_version or '',
+                self.platform or '',
+            )
+            return result
+
+    setattr(Distribution, 'hashcmp', property(hashcmp))
 
 
 patch_Distribution()
@@ -72,119 +72,6 @@ def patch_PackageIndex():
         from pip._internal.models.target_python import TargetPython
         from pip._vendor import six
         from pip._vendor.six.moves import urllib
-
-        PY_VERSION_INFO = TargetPython().py_version_info
-
-        # method copied over from setuptools 46.1.3
-        # Unchanged in setuptools 70.0.0.
-        def process_url(self, url, retrieve=False):
-            """Evaluate a URL as a possible download, and maybe retrieve it"""
-            if url in self.scanned_urls and not retrieve:
-                return
-            self.scanned_urls[url] = True
-            if not URL_SCHEME(url):
-                self.process_filename(url)
-                return
-            else:
-                dists = list(distros_for_url(url))
-                if dists:
-                    if not self.url_ok(url):
-                        return
-                    self.debug("Found link: %s", url)
-
-            if dists or not retrieve or url in self.fetched_urls:
-                list(map(self.add, dists))
-                return  # don't need the actual page
-
-            if not self.url_ok(url):
-                self.fetched_urls[url] = True
-                return
-
-            self.info("Reading %s", url)
-            self.fetched_urls[url] = True  # prevent multiple fetch attempts
-            tmpl = "Download error on %s: %%s -- Some packages may not be found!"
-            f = self.open_url(url, tmpl % url)
-            if f is None:
-                return
-            if isinstance(f, urllib.error.HTTPError) and f.code == 401:
-                self.info("Authentication error: %s" % f.msg)
-            self.fetched_urls[f.url] = True
-            if 'html' not in f.headers.get('content-type', '').lower():
-                f.close()  # not html, we can't process it
-                return
-
-            base = f.url  # handle redirects
-            page = f.read()
-
-            # --- LOCAL CHANGES MADE HERE: ---
-
-            if isinstance(page, six.text_type):
-                page = page.encode('utf8')
-                charset = 'utf8'
-            else:
-                if isinstance(f, urllib.error.HTTPError):
-                    # Errors have no charset, assume latin1:
-                    charset = 'latin-1'
-                else:
-                    try:
-                        charset = f.headers.get_param('charset') or 'latin-1'
-                    except AttributeError:
-                        # Python 2
-                        charset = f.headers.getparam('charset') or 'latin-1'
-
-            try:
-                content_type = f.getheader('content-type')
-            except AttributeError:
-                # On at least Python 2.7:
-                # addinfourl instance has no attribute 'getheader'
-                content_type = "text/html"
-
-            try:
-                # pip 22.2+
-                html_page = IndexContent(
-                    page,
-                    content_type=content_type,
-                    encoding=charset,
-                    url=base,
-                    cache_link_parsing=False,
-                )
-            except TypeError:
-                try:
-                    # pip 20.1-22.1
-                    html_page = IndexContent(page, charset, base, cache_link_parsing=False)
-                except TypeError:
-                    # pip 20.0 or older
-                    html_page = IndexContent(page, charset, base)
-
-            # https://github.com/buildout/buildout/issues/598
-            # use_deprecated_html5lib is a required addition in pip 22.0/22.1
-            # and it is gone already in 22.2
-            try:
-                plinks = parse_links(html_page, use_deprecated_html5lib=False)
-            except TypeError:
-                plinks = parse_links(html_page)
-            plinks = list(plinks)
-
-            # --- END OF LOCAL CHANGES ---
-
-            if not isinstance(page, str):
-                # In Python 3 and got bytes but want str.
-                page = page.decode(charset, "ignore")
-            f.close()
-
-            # --- LOCAL CHANGES MADE HERE: ---
-
-            for link in plinks:
-                if _check_link_requires_python(link, PY_VERSION_INFO):
-                    self.process_url(link.url)
-
-            # --- END OF LOCAL CHANGES ---
-
-            if url.startswith(self.index_url) and getattr(f, 'code', None) != 404:
-                page = self.process_index(url, page)
-
-        setattr(PackageIndex, 'process_url', process_url)
-
     except ImportError:
         import logging
         logger = logging.getLogger('zc.buildout.patches')
@@ -194,6 +81,118 @@ def patch_PackageIndex():
             exc_info=True
         )
         return
+
+    PY_VERSION_INFO = TargetPython().py_version_info
+
+    # method copied over from setuptools 46.1.3
+    # Unchanged in setuptools 70.0.0.
+    def process_url(self, url, retrieve=False):
+        """Evaluate a URL as a possible download, and maybe retrieve it"""
+        if url in self.scanned_urls and not retrieve:
+            return
+        self.scanned_urls[url] = True
+        if not URL_SCHEME(url):
+            self.process_filename(url)
+            return
+        else:
+            dists = list(distros_for_url(url))
+            if dists:
+                if not self.url_ok(url):
+                    return
+                self.debug("Found link: %s", url)
+
+        if dists or not retrieve or url in self.fetched_urls:
+            list(map(self.add, dists))
+            return  # don't need the actual page
+
+        if not self.url_ok(url):
+            self.fetched_urls[url] = True
+            return
+
+        self.info("Reading %s", url)
+        self.fetched_urls[url] = True  # prevent multiple fetch attempts
+        tmpl = "Download error on %s: %%s -- Some packages may not be found!"
+        f = self.open_url(url, tmpl % url)
+        if f is None:
+            return
+        if isinstance(f, urllib.error.HTTPError) and f.code == 401:
+            self.info("Authentication error: %s" % f.msg)
+        self.fetched_urls[f.url] = True
+        if 'html' not in f.headers.get('content-type', '').lower():
+            f.close()  # not html, we can't process it
+            return
+
+        base = f.url  # handle redirects
+        page = f.read()
+
+        # --- LOCAL CHANGES MADE HERE: ---
+
+        if isinstance(page, six.text_type):
+            page = page.encode('utf8')
+            charset = 'utf8'
+        else:
+            if isinstance(f, urllib.error.HTTPError):
+                # Errors have no charset, assume latin1:
+                charset = 'latin-1'
+            else:
+                try:
+                    charset = f.headers.get_param('charset') or 'latin-1'
+                except AttributeError:
+                    # Python 2
+                    charset = f.headers.getparam('charset') or 'latin-1'
+
+        try:
+            content_type = f.getheader('content-type')
+        except AttributeError:
+            # On at least Python 2.7:
+            # addinfourl instance has no attribute 'getheader'
+            content_type = "text/html"
+
+        try:
+            # pip 22.2+
+            html_page = IndexContent(
+                page,
+                content_type=content_type,
+                encoding=charset,
+                url=base,
+                cache_link_parsing=False,
+            )
+        except TypeError:
+            try:
+                # pip 20.1-22.1
+                html_page = IndexContent(page, charset, base, cache_link_parsing=False)
+            except TypeError:
+                # pip 20.0 or older
+                html_page = IndexContent(page, charset, base)
+
+        # https://github.com/buildout/buildout/issues/598
+        # use_deprecated_html5lib is a required addition in pip 22.0/22.1
+        # and it is gone already in 22.2
+        try:
+            plinks = parse_links(html_page, use_deprecated_html5lib=False)
+        except TypeError:
+            plinks = parse_links(html_page)
+        plinks = list(plinks)
+
+        # --- END OF LOCAL CHANGES ---
+
+        if not isinstance(page, str):
+            # In Python 3 and got bytes but want str.
+            page = page.decode(charset, "ignore")
+        f.close()
+
+        # --- LOCAL CHANGES MADE HERE: ---
+
+        for link in plinks:
+            if _check_link_requires_python(link, PY_VERSION_INFO):
+                self.process_url(link.url)
+
+        # --- END OF LOCAL CHANGES ---
+
+        if url.startswith(self.index_url) and getattr(f, 'code', None) != 404:
+            page = self.process_index(url, page)
+
+    setattr(PackageIndex, 'process_url', process_url)
 
 
 patch_PackageIndex()
@@ -211,14 +210,19 @@ def patch_interpret_distro_name():
     The new code correctly handles this as project name
     ''mauritstest.namespacepackage', instead of yielding multiple distros.
     """
-    # from packaging.version import Version, parse
-    # if setuptools.__version__ < 70
-    # XXX
-    from pkg_resources import Distribution
-    from pkg_resources import SOURCE_DIST
+    try:
+        from zc.buildout._compat import version
+        from pkg_resources import Distribution
+        from pkg_resources import SOURCE_DIST
 
-    import re
-    import setuptools
+        import re
+        import setuptools
+    except ImportError:
+        return
+
+    if version.parse(setuptools.__version__) >= version.Version("70"):
+        # Patch is not needed.
+        return
 
     def interpret_distro_name(
         location, basename, metadata, py_version=None, precedence=SOURCE_DIST, platform=None
@@ -266,10 +270,13 @@ def patch_pkg_resources_requirement_contains():
     a Distribution, without the key needing to be exactly the same.
     We want to compare normalized names.
     """
-    from pkg_resources import Distribution
-    from pkg_resources import Requirement
-    from zc.buildout.utils import normalize_name
-    from zc.buildout._compat import version
+    try:
+        from pkg_resources import Distribution
+        from pkg_resources import Requirement
+        from zc.buildout.utils import normalize_name
+        from zc.buildout._compat import version
+    except ImportError:
+        return
 
     def __contains__(self, item):
         if isinstance(item, Distribution):
