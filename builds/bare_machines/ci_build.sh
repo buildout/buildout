ab16b17bf8688b625d91559f2ad63f3eeaadcc26
Maurits van Rees <maurits@vanrees.org>
2024-08-23 00:02:37 +0200

    CI: call python -mpip
    On CircleCI I see:
    
    ```
    + pip3.11 uninstall -y six
    ./ci_build.sh: line 10: pip3.11: command not found
    ```
    
    It continues, but seems better this way anyway.

--- builds/bare_machines/ci_build.sh
+++ builds/bare_machines/ci_build.sh
@@ -7,6 +7,6 @@ cd ../..
 
 # Some machines come with preinstalled six
 # which breaks test suite
-pip${PYTHON_VER} uninstall -y six || true
+python${PYTHON_VER} -mpip uninstall -y six || true
 python${PYTHON_VER} dev.py
 make -f Makefile.builds test_without_coverage
