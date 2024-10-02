fe442027ddc21bf9ff7f0444b45cc9a759787a82
Maurits van Rees <maurits@vanrees.org>
2024-10-02 18:03:18 +0200

    Remove ci_build.sh, builds directory, Makefile.buildout/configure, and Dockerfiles.

--- builds/debian_containers/ci_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x
-set -e
-
-cd ../..
-docker build -f .github/workflows/Dockerfile-debian --tag debian_buildout:python${PYTHON_VER} --build-arg PYTHON_VER=${PYTHON_VER} .
-docker run "debian_buildout:python${PYTHON_VER}" make -f Makefile.builds test_with_coverage
--- builds/debian_sys_container/ci_build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x
-set -e
-
-cd ../..
-docker build -f .github/workflows/Dockerfile-debian-system --tag debian_system_buildout .
-docker run debian_system_buildout make -f Makefile.builds test_with_coverage
