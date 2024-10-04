f86e44de40e37a881accf8dce377dc1d93b1190b
Maurits van Rees <maurits@vanrees.org>
2024-10-04 11:56:34 +0200

    Support Python 3.12 and 3.13.
    This only needed a few test fixes.
    Tested with Python 3.13.0rc3.

--- setup.py
+++ setup.py
@@ -69,6 +69,7 @@ setup(
        'Programming Language :: Python :: 3.10',
        'Programming Language :: Python :: 3.11',
        'Programming Language :: Python :: 3.12',
+       'Programming Language :: Python :: 3.13',
        'Topic :: Software Development :: Build Tools',
        'Topic :: Software Development :: Libraries :: Python Modules',
        ],
