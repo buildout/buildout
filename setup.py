3759c5858b8cd09921a335ad370afbec7a86d3e4
Michael Howitz <icemac@gmx.net>
2025-01-30 08:45:36 +0100

    Back to development: 4.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 name = "zc.buildout"
-version = '4.0'
+version = '4.1.dev0'
 
 import os
 from setuptools import setup
