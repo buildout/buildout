3d5c20400de6889c008741495b12d63d7879b994
Maurits van Rees <maurits@vanrees.org>
2025-03-04 17:21:14 +0100

    Back to development: 4.1.2
    [ci skip]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 name = "zc.buildout"
-version = '4.1.1'
+version = '4.1.2.dev0'
 
 import os
 from setuptools import setup
