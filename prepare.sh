a90590fd39467932275bdbdbbc5cabab649b7e5b
Maurits van Rees <maurits@vanrees.org>
2025-04-09 11:39:51 +0200

    Use wheel 0.45.1 when testing with setuptools older than 70.1.0.
    Otherwise, when combining an older `setuptools` with a newer `wheel` version, the `bdist_wheel` command exists in neither of these packages.
    
    Fixes https://github.com/buildout/buildout/issues/705

--- prepare.sh
+++ prepare.sh
@@ -61,16 +61,37 @@ if test $PIP_VERSION; then
     # Make this '-U pip==version'.
 	PIP_ARGS="$PIP_ARGS==$PIP_VERSION"
 fi
+WHEEL_VERSION=""
 PIP_ARGS="$PIP_ARGS setuptools"
 if test $SETUPTOOLS_VERSION; then
 	PIP_ARGS="$PIP_ARGS==$SETUPTOOLS_VERSION"
+  # wheel is a dependency of zc.buildout, but we may need a specific version.
+  # If we use setuptools older than 70.1.0, we must use at most wheel 0.45.1,
+  # otherwise the bdist_wheel command is not present.
+  # Sorry, this will be some ugly bash stuff.
+  # 1. Get rid of the first dot in the version: 69.5.1 -> 695.1
+  SV="${SETUPTOOLS_VERSION/./}"
+  # 2. Take only the first three characters: 695.1 -> 695
+  SV="${SV:0:3}"
+  # 3. Do the same two transformations on the target version:
+  # 70.1.0 -> 701.0 -> 701.  Check if the SETUPTOOLS_VERSION is lighter.
+  if test $SV -lt 701; then
+    WHEEL_VERSION="0.45.1"
+    echo "Pinning wheel to $WHEEL_VERSION for compat with older setuptools."
+  fi
 fi
-# wheel and packaging are already dependencies of zc.buildout.
+# Always include wheel:
+PIP_ARGS="$PIP_ARGS wheel"
+if test $WHEEL_VERSION; then
+  # Use a specific version:
+	PIP_ARGS="$PIP_ARGS==$WHEEL_VERSION"
+fi
+# packaging is already a dependency of zc.buildout, but we explicitly add it.
 # We add 'build' so we can build a source dist of zc.buildout,
 # which has a side effect we need: generate 'src/zc.buildout.egg-info'
 # This is needed so in Python we can do:
 # >>> pkg_resources.working_set.add_entry('src')
-PIP_ARGS="$PIP_ARGS wheel packaging build"
+PIP_ARGS="$PIP_ARGS packaging build"
 echo
 echo "Using arguments for pip install: $PIP_ARGS"
 # "$VENV_PYTHON" -m pip install -e .[test] -e zc.recipe.egg_[test] $PIP_ARGS
