9d698759329e0daf23bfd6dd2ff795d4a8a097e8
Maurits van Rees <maurits@vanrees.org>
2025-06-04 09:54:34 +0200

    Copy more_itertools.unique_everseen to package_index.py.
    I don't want to add a dependency just for one function.
    Depending on your setuptools version, this may or may not be needed.
    setuptools 57 vendorizes it.
    But I see CI fail until setuptools 69, so likely this function does not exist in older versions.

--- src/zc/buildout/_package_index.py
+++ src/zc/buildout/_package_index.py
@@ -47,8 +47,6 @@ from fnmatch import translate
 from functools import wraps
 from typing import NamedTuple
 
-from more_itertools import unique_everseen
-
 import setuptools
 from pkg_resources import (
     BINARY_DIST,
@@ -92,6 +90,55 @@ __all__ = [
 _SOCKET_TIMEOUT = 15
 
 user_agent = f"setuptools/{setuptools.__version__} Python-urllib/{sys.version_info.major}.{sys.version_info.minor}"
+
+
+
+def unique_everseen(iterable, key=None):
+    """Yield unique elements, preserving order.
+
+    This function is copied from `more_itertools` 10.7.0.  I did not want to
+    add `more_itertools` as dependency for just this function.
+    `more-itertools` is under the MIT License.
+
+        >>> list(unique_everseen('AAAABBBCCDAABBB'))
+        ['A', 'B', 'C', 'D']
+        >>> list(unique_everseen('ABBCcAD', str.lower))
+        ['A', 'B', 'C', 'D']
+
+    Sequences with a mix of hashable and unhashable items can be used.
+    The function will be slower (i.e., `O(n^2)`) for unhashable items.
+
+    Remember that ``list`` objects are unhashable - you can use the *key*
+    parameter to transform the list to a tuple (which is hashable) to
+    avoid a slowdown.
+
+        >>> iterable = ([1, 2], [2, 3], [1, 2])
+        >>> list(unique_everseen(iterable))  # Slow
+        [[1, 2], [2, 3]]
+        >>> list(unique_everseen(iterable, key=tuple))  # Faster
+        [[1, 2], [2, 3]]
+
+    Similarly, you may want to convert unhashable ``set`` objects with
+    ``key=frozenset``. For ``dict`` objects,
+    ``key=lambda x: frozenset(x.items())`` can be used.
+
+    """
+    seenset = set()
+    seenset_add = seenset.add
+    seenlist = []
+    seenlist_add = seenlist.append
+    use_key = key is not None
+
+    for element in iterable:
+        k = key(element) if use_key else element
+        try:
+            if k not in seenset:
+                seenset_add(k)
+                yield element
+        except TypeError:
+            if k not in seenlist:
+                seenlist_add(k)
+                yield element
 
 
 def parse_requirement_arg(spec):
