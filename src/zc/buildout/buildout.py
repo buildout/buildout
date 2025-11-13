614b98ccb49721c512649b981a005a64dadc3584
Maurits van Rees <maurits@vanrees.org>
2025-11-13 21:43:49 +0100

    Debug when project gets upgraded.
    This happens too often for horse-with-no-namespace.

--- src/zc/buildout/buildout.py
+++ src/zc/buildout/buildout.py
@@ -1173,9 +1173,7 @@ class Buildout(DictMixin):
         # the true `wheel` package and the one vendorized by `setuptools`.
         # See https://github.com/buildout/buildout/issues/691
         # We explicitly install all our dependencies here.
-        # They must be importable, so we specify 'horse_with_no_namespace'
-        # instead of 'horse-with-no-namespace'.
-        projects = ('zc.buildout', 'wheel', 'pip', 'setuptools', 'horse_with_no_namespace')
+        projects = ('zc.buildout', 'wheel', 'pip', 'setuptools', 'horse-with-no-namespace')
         ws = zc.buildout.easy_install.install(
             projects,
             self['buildout']['eggs-directory'],
@@ -1196,6 +1194,7 @@ class Buildout(DictMixin):
                 # version is used, this is either useless or a life saver.
                 req = pkg_resources.Requirement.parse(project)
                 dist = ws.find(req)
+            project = project.replace('-', '_')
             importlib.import_module(project)
             if dist is None:
                 # This is unexpected.  This must be some problem with how we use
@@ -1207,6 +1206,12 @@ class Buildout(DictMixin):
                 )
                 continue
             if not inspect.getfile(sys.modules[project]).startswith(dist.location):
+                project_file = inspect.getfile(sys.modules[project])
+                self._logger.warning("Module %s is loaded from %s, but distribution is at %s.",
+                                     project, project_file, dist.location)
+                # In tests, the normalizers make manually inspecting paths impossible.
+                self._logger.warning("project file: %r", [ord(x) for x in project_file])
+                self._logger.warning("dist location: %r", [ord(x) for x in dist.location])
                 upgraded.append(dist)
 
         if not upgraded:
